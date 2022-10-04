73ad675661364ce6e47562b8b01cf945a58bd694
Joni Fält <rikufalt@gmail.com>
2022-10-04 18:46:42 +0300

    Securing web application part2

--- src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -9,6 +9,8 @@ import com.example.Bookstore.domain.Book;
 import com.example.Bookstore.domain.BookRepository;
 import com.example.Bookstore.domain.Category;
 import com.example.Bookstore.domain.CategoryRepository;
+import com.example.Bookstore.domain.UserRepository;
+import com.example.Bookstore.domain.Users;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -18,7 +20,8 @@ public class BookstoreApplication {
 	}
 
 	@Bean
-	public CommandLineRunner demo(BookRepository brepository, CategoryRepository crepository) {
+	public CommandLineRunner demo(BookRepository brepository, CategoryRepository crepository,
+			UserRepository urepository) {
 		return (args) -> {
 
 			crepository.save(new Category("Horror"));
@@ -33,6 +36,13 @@ public class BookstoreApplication {
 
 			brepository.save(new Book("Game of thrones", "George R. R. Martin", "234-22", "1978", 21,
 					crepository.findByName("History").get(0)));
+
+			Users user1 = new Users("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6",
+					"testi@testi.com", "USER");
+			Users user2 = new Users("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C",
+					"testi1@testi1.com", "ADMIN");
+			urepository.save(user1);
+			urepository.save(user2);
 
 		};
 	}
